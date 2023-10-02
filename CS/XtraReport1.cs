e59e397cb6af9f97d009f306758fcf8bfe72d8d6
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 13:26:01 +0400

    Change example status to out of maintenance

--- CS/XtraReport1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WebApplication1 {
-    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
-        public XtraReport1() {
-            InitializeComponent();
-        }
-    }
-}
